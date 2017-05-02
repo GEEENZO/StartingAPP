e7879ded9af693b46ecbfb70b9aee92235078b6e
gzn <ochimusha.jp@gmail.com>
2017-05-02 18:22:46 +0900

    startingAPP changes BROADCAST-APP ver.1.1

--- app/src/main/java/com/exmple/tools/startingapp/MyBroadcastReceiver.java
+++ app/src/main/java/com/exmple/tools/startingapp/MyBroadcastReceiver.java
@@ -3,16 +3,36 @@ package com.exmple.tools.startingapp;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import static android.content.ContentValues.TAG;
 
 /**
  * Created by user1 on 2017/05/02.
  */
 
 public class MyBroadcastReceiver extends BroadcastReceiver{
+
+    TextView textView;
+    //Context context;
     @Override
     public void onReceive(Context context, Intent intent) {
-        String str = intent.getExtras().getString("test");
+
+            Log.d(TAG,"onReceiveSTARTING:");
+        String str = "use?";
+        str = intent.getExtras().getString("backs");
             Log.i("tag",str);
+            Toast.makeText(context,str, Toast.LENGTH_SHORT).show();
+
+        Bundle bundle = intent.getExtras();
+
+        if(bundle == null){
+            Log.i("TAG::","NULL--ON/"+str);
+        }else{
+            Log.i("TAG::",bundle.getString("button")+"ADB-ON「"+str+"」");
+        }
     }
 }
